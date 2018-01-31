59b7ae411b54fadd735a41a03b730edda1942c67
HanKwangHee <house9737@gmail.com>
2018-01-31 16:27:49 +0900

    ip_notifier added

--- mqtt_messanger/ip_notifier.py
+++ mqtt_messanger/ip_notifier.py
@@ -28,9 +28,9 @@ class ip_notifier:
 			)[20:24])
 
 	def publish_ip_addr(self):
-		# self.get_ip_addr('wlan0')
+		self.get_ip_addr('wlan0')
 		self.client.connect(pfc_conf.PFC_BROKER_HOST, pfc_conf.PFC_BROKER_PORT,pfc_conf.PFC_BROKER_KEEPALIVE)
-		# self.client.publish(self.publish_topic, self.ip_addr, self.pfc_alias)
+		self.client.publish(self.publish_topic, self.ip_addr, self.pfc_alias)
 		self.client.publish(self.publish_topic, 'Meesage sented by ip_notifier - ' + self.pfc_alias + '/' + str(datetime.now()))
 		self.client.disconnect()
 
