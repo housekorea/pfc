3b924bf971ac0bb999c06b4216128cae2ebbe054
HanKwangHee <house9737@gmail.com>
2018-02-12 14:47:44 +0900

    Subscriber_order implement

--- AWS_IOT/subscriber_order.py
+++ AWS_IOT/subscriber_order.py
@@ -4,6 +4,7 @@ import os
 import sys
 from datetime import datetime
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
 import time
 import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -12,6 +13,15 @@ from configure import pfc_mqtt_topic
 from command_mapper import command_mapper
 import subprocess, shlex
 from threading import Timer
+
+# Configure logging
+logging.basicConfig(filename=pfc_conf.LOG_DIR_PATH + '/aws_iot_log.log')
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
 
 
 class subscriber_order:
@@ -50,12 +60,17 @@ class subscriber_order:
 
 
 	def order_callback(self, om_type, om_target, om_order):
+
+		kill_proc = lambda p: p.kill()
+
 		if om_type == 'SENSOR':
 			if om_target in command_mapper.SENSOR and om_order in command_mapper.SENSOR[om_target]:
 				command_pfc_sensor = command_mapper.SENSOR_DIR_PATH +command_mapper.SENSOR[om_target][om_order]
+
 				print(command_pfc_sensor)
+				# Execute get sensor data python process through subprocess
+				# It has a timeout setting to prevent permanent blocking
 				sensor_proc = subprocess.Popen(shlex.split("python " + command_pfc_sensor), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-				kill_proc = lambda p: p.kill()
 				timer = Timer(30, kill_proc,[sensor_proc])
 				try :
 					timer.start()
@@ -63,21 +78,57 @@ class subscriber_order:
 				finally:
 					timer.cancel()
 
-				sensor_data = {'data' :stdout , 'PFC_SERIAL' :'239412', 'DEVICE_DT' : str(datetime.now())}
-				pub_proc = subprocess.Popen(shlex.split("python publisher_sensor_data.py -t 'EZFARM/PFC/V1/DEV' -m \"" +str(sensor_data) + '"'))
-
-
-				# SENSOR_DATA = subprocess.check_output("python " + command_pfc_sensor,shell=True)
-				# print(SENSOR_DATA)
+				# Publish sensor data to AWS IOT DEVICE GATEWAY
+				sensor_data = {'data' :stdout , 'PFC_SERIAL' :str(pfc_conf.PFC_AWS_IOT_SERIAL), 'DEVICE_DT' : str(datetime.now())}
+				pub_proc = subprocess.Popen(shlex.split("python publisher_sensor_data.py -t '" + pfc_mqtt_topic.PUBLISH_SENSOR+ "' -m \"" +str(sensor_data) + '"'))
+				timer = Timer(30,kill_proc, [pub_proc])
+				try :
+					timer.start()
+					stdout,stderr = pub_proc.communicate()
+				finally :
+					timer.cancel()
 
 		elif om_type == 'ACTUATOR':
-			None
+			if om_target in command_mapper.ACTUATOR and om_order in command_mapper.ACTUATOR[om_target]:
+				command_pfc_actuator = command_mapper.SENSOR_DIR_PATH + command_mapper.ACTUATOR[om_target][om_order]
+
+				print(command_pfc_actuator)
+				# Execute get sensor data python process through subprocess
+				# It has a timeout setting to prevent permanent blocking
+				actuator_proc = subprocess.Popen(shlex.split("python " + command_pfc_actuator), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				timer = Timer(30, kill_proc,[actuator_proc])
+				try :
+					timer.start()
+					stdout.stderr = actuator_proc.communicate()
+				finally :
+					timer.cancel()
+
+				actuator_data = {'data':stdout, 'PFC_SERIAL': str(pfc_conf.PFC_AWS_IOT_SERIAL), 'DEVICE_DT' : str(datetime.now())}
+				pub_proc = subprocess.Popen(shlex.split("python publisher_actuator_data.py -t '"+pfc_mqtt_topic.PUBLISH_ACTUATOR+"' -m \"" +str(actuator_data) + '"'))
+				timer = Timer(30,kill_proc, [pub_proc])
+				try :
+					timer.start()
+					stdout,stderr = pub_proc.communicate()
+				finally :
+					timer.cancel()
+
 		elif om_type == 'LOCAL_IP' :
-			None
+			pub_proc = subprocess.Popen(shlex.split("python publisher_local_ip.py"))
+			Timer = Timer(30,kill_proc, [pub_proc])
+			try :
+				timer.start()
+				stdout,stderr = pub_proc.communicate()
+			finally :
+				timer.cancel()
+
 		elif om_type == 'HEARTBEAT' :
-			None
-
-
+			pub_proc = subprocess.Popen(shlex.split("python publisher_heartbeat.py"))
+			Timer = Timer(30, kill_proc, [pub_proc])
+			try :
+				timer.start()
+				stdout, stderr = pub_proc.communicate()
+			finally :
+				timer.cancel()
 
 
 	def subscribe_mqtt_broker(self):
